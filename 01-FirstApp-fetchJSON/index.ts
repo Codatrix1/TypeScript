f2fc489829e68cb7fc73c5c16c5c8d8d485990e6
Codatrix <ankur.codatrix@gmail.com>
2022-03-12 12:29:12 +0530

    Catching Errors with TypeScript

--- 01-FirstApp-fetchJSON/index.ts
+++ 01-FirstApp-fetchJSON/index.ts
@@ -3,6 +3,26 @@ import axios from "axios";
 // API url address to fetch data
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+};
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
 });
