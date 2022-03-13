5b1589373aac6f434c04ed3692f300f320ff423c
Codatrix <ankur.codatrix@gmail.com>
2022-03-13 10:14:21 +0530

    What is a Type System? w/ Basic Examples

--- 01-FirstApp-fetchJSON/index.ts
+++ 01-FirstApp-fetchJSON/index.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 // API url address to fetch data
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+// interface defined a new type here as Todo
 interface Todo {
   id: number;
   title: string;
---
+++ 02-Features/types.ts
@@ -0,0 +1,13 @@
+// variable example
+const today = new Date();
+today.getMonth();
+
+// object example
+const person = {
+  age: 20,
+};
+person.age;
+
+// class example
+class Color {}
+const red = new Color();
