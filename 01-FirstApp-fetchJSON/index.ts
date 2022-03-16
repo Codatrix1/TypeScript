a387c8b19fb0bf38a48accd1832a155590ea0295
Codatrix <ankur.codatrix@gmail.com>
2022-03-16 21:05:36 +0530

    Details Added as Comments

--- 01-FirstApp-fetchJSON/index.ts
+++ 01-FirstApp-fetchJSON/index.ts
@@ -3,13 +3,14 @@ import axios from "axios";
 // API url address to fetch data
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
-// interface defined a new type here as Todo
+// interface Todo: Used to define the structure of an object
 interface Todo {
   id: number;
   title: string;
   completed: boolean;
 }
 
+// Helper function
 const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The todo with ID: ${id}
@@ -18,6 +19,7 @@ const logTodo = (id: number, title: string, completed: boolean) => {
   `);
 };
 
+// Main Application
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
 
