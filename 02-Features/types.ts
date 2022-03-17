6fbd1415de043731481ec80caf2099c182dd7c12
Codatrix <ankur.codatrix@gmail.com>
2022-03-17 10:08:21 +0530

    Few details added

--- 02-Features/types.ts
+++ 02-Features/types.ts
@@ -1,13 +1,27 @@
-// variable example
+//----------------------
+// Types in TypeScript
+//---------------------
+
+// PRIMITIVE Types -> [STRING, NUMBER, BOOLEAN,  UNDEFINED, NULL, SYMBOL, VOID]
+// OBJECT Types -> [OBJECTS, CLASSES, ARRAYS, FUNCTIONS]
+
+// ⭐ Important Note: About Object Types
+// We can trick the typescript compiler in a good way to categorize even the Object Types a different types
+
+//----------------
+// Some Examples
+//----------------
+
+// Date Example
 const today = new Date();
 today.getMonth();
 
-// object example
+// Object Example
 const person = {
   age: 20,
 };
 person.age;
 
-// class example
+// Class Example
 class Color {}
 const red = new Color();
