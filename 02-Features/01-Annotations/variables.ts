a0e940ce38558b53b44f953826f1efbc6e33df74
Codatrix <ankur.codatrix@gmail.com>
2022-03-17 12:05:31 +0530

    Function Annotations - Understanding Inference

--- 02-Features/01-Annotations/variables.ts
+++ 02-Features/01-Annotations/variables.ts
@@ -1,9 +1,10 @@
+// ⭐ Type Annotations --> We as Developers have to specify the types manually for TypeScript
 //-----------------------------
 // Annotations with Primitives
 //-----------------------------
 let apples: number = 5;
 let firstName: string = "Ankur";
-let hasName: boolean = true;
+let hasFirstName: boolean = true;
 
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
@@ -30,3 +31,18 @@ let coords: { lat: number; lng: number } = {
   lat: 31.0675,
   lng: 81.3119,
 };
+
+//---------------------------
+// Annotations with Functions
+//---------------------------
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// ⭐ Type Inference --> TypeScript itself guesses the types
+//-----------------------------
+// Inference with Primitives
+//-----------------------------
+let bananas = 20;
+let lastName = "Sinha";
+let hasLastName = true;
